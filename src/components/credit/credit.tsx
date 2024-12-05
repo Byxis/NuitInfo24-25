0728341a4f17902ea5450ed9f750db93809afb7c
Maxence-Esteban CHATEAU <maxence-esteban.chateau@etu.umontpellier.fr>
2024-12-05 21:53:44 +0100

    Creation des cartes utilisateurs dans l'optique d'une page de credit

---
+++ src/components/credit/credit.tsx
@@ -0,0 +1,16 @@
+import UserCard from "./Card/UserCard.tsx"
+
+function Credit() {
+
+  return (
+    <div>
+        <UserCard username="MaxbanCh"> </UserCard>
+        <UserCard username="Byxis"> </UserCard>
+        <UserCard username="yaya1541"> </UserCard>
+        <UserCard username="dawoldo"> </UserCard>
+        <UserCard username="Askneuh"> </UserCard>
+    </div>
+  )
+}
+
+export default Credit
