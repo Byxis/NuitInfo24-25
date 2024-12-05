af470ba9edf54b1921e2c314bd21b4e92f380878
Sebastien <askneuh@gmail.com>
2024-12-05 23:15:36 +0100

    HomePage & NavBar prototype

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,22 @@
+import { Route, HashRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
+import Home from '../Homepage/Home'
+import Layout from './Layout'
+import Test from '../Fichier de test/test'
+
 
 function App() {
 
   return (
     <>
+    <Router>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/test" element={<Test/>}/>
+        </Route>
+      </Routes>
+    </Router>
     </>
   )
 }
