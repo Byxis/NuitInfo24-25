d5028d5c70f043a72a1a5a1bc9ca5f97abf4b044
alexis.serrano <alexis.serrano@etu.umontpellier.fr>
2024-12-06 07:18:04 +0100

    fix des imports

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,5 @@
-import { Route, HashRouter as Router, Routes } from 'react-router-dom'
 import CarteInteractive from '../CarteInteractive/CarteInteractive.tsx'
-import Coeur from '../Coeur/Coeur.tsx'
-import CourantsOceaniques from '../Coeur/CourantsOceaniques.tsx'
-import Poumons from '../Poumons/Poumons.tsx'
-import Plancton from '../Poumons/Plancton.tsx'
-import SystemeDigestif from '../SystemeDigestif/SystemeDigestif.tsx'
-import ReseauTrophique from '../SystemeDigestif/ReseauTrophique.tsx'
-import SystemeImmunitaire from '../SystemeImmunitaire/SystemeImmunitaire.tsx'
-import RecifCorallien from '../SystemeImmunitaire/RecifCorallien.tsx'
-import Peau from '../Peau/Peau.tsx'
-import Reins from '../Reins/Reins.tsx'
-import Surface from '../Peau/Surface.tsx'
-import Mangroves from '../Reins/Mangroves.tsx'
-import Sang from '../Sang/Sang.tsx'
-import Eau from '../Sang/Eau.tsx'
-import VaisseauxSanguins from '../VaisseauxSanguins/VaisseauxSanguins.tsx'
-import RivieresFleuves from '../VaisseauxSanguins/RivieresFleuves.tsx'
 import { useState } from 'react'
-import Layout from '../Layout.tsx'
 
 
 function App2() {
