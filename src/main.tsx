cc3b9701e9adc98ab6789c1a18c3cf95f52ff57f
Maxence-Esteban CHATEAU <maxence-esteban.chateau@etu.umontpellier.fr>
2024-12-06 04:31:10 +0100

    pour merge request

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Credit from './components/credit/credit'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Credit />
+    <App />
   </StrictMode>,
 )
