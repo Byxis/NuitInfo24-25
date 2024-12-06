26bf1836c8f4a570edb63c6812e25e3b5b9ad94b
Sebastien <askneuh@gmail.com>
2024-12-06 02:07:25 +0100

    Footer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,28 +1,15 @@
 import { Route, HashRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
-<<<<<<< HEAD
-import GetQuestion from '../Quiz/questions';
-import Quiz from '../Quiz/Quiz';
-=======
-import Home from '../Homepage/Home'
 import Layout from './Layout'
 import Test from '../Fichier de test/test'
+import Footer from '../Footer/Footer'
+import Home from './Homepage/Home'
 
->>>>>>> af470ba (HomePage & NavBar prototype)
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
-      <div>
-        <div>
-          <p id="score">Score : 0</p>
-        </div>
-        <Quiz {...GetQuestion(0)} />
-        <Quiz {...GetQuestion(0)} />
-      </div>
-=======
     <Router>
       <Routes>
         <Route element={<Layout/>}>
@@ -31,7 +18,7 @@ function App() {
         </Route>
       </Routes>
     </Router>
->>>>>>> af470ba (HomePage & NavBar prototype)
+    <Footer/>
     </>
   );
 }
